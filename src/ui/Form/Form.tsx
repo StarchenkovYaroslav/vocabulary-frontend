05b6f99f8ec152693785139646f4b683f77aa617
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-02-01 00:23:33 +0600

    fix: fix form children type

--- src/ui/Form/Form.tsx
+++ src/ui/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import { Form as FinalForm, FormProps } from 'react-final-form'
 
 interface Props<FormValues> extends FormProps<FormValues> {
@@ -9,13 +9,12 @@ function Form<FormValues>({
   onSubmit,
   className,
   children,
-}: Props<FormValues>) {
+}: PropsWithChildren<Props<FormValues>>) {
   return (
     <FinalForm<FormValues> onSubmit={onSubmit}>
       {({ handleSubmit }) => (
         <form className={className} onSubmit={handleSubmit}>
-          {/*TODO: type*/}
-          {children as React.ReactNode}
+          {children}
         </form>
       )}
     </FinalForm>
